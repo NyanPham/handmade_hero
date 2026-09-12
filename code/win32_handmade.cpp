a53d380a204a6a477467251e47c239e196d8967e
phamthanhnhanussh@gmail.com <phamthanhnhanussh@gmail.com>
2026-09-12 16:29:42 +0700

    Day 003 - Allocating a Backbuffer

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,11 +1,65 @@
 #include "windows.h"
 #include <stdio.h>
 
+#define internal static
+#define local_persist static
+#define global_variable static
+
+// TODO: This is a global for now
+global_variable bool Running;
+
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
+
+internal void
+Win32ResizeDIBSection(int Width, int Height)
+{
+    // TODO: Bulletproof this
+    // Maybe don't free first, free after, then free first if that fails.
+
+    if (BitmapHandle)
+    {
+        DeleteObject(BitmapHandle);
+    }
+
+    if (!BitmapDeviceContext)
+    {
+        // TODO: Should we recreate these under certain circumstances
+        BitmapDeviceContext = CreateCompatibleDC(0);
+    }
+
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = Width;
+    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+    BitmapHandle = CreateDIBSection(
+        BitmapDeviceContext, &BitmapInfo,
+        DIB_RGB_COLORS,
+        &BitmapMemory,
+        0, 0);
+}
+
+internal void
+Win32UpdateWindow(HDC Devicecontext, int X, int Y, int Width, int Height)
+{
+    StretchDIBits(Devicecontext,
+                  X, Y, Width, Height,
+                  X, Y, Width, Height,
+                  BitmapMemory,
+                  &BitmapInfo,
+                  DIB_RGB_COLORS, SRCCOPY);
+}
+
 LRESULT CALLBACK
-MainWindowCallback(HWND Window,
-                   UINT Message,
-                   WPARAM WParam,
-                   LPARAM LParam)
+Win32MainWindowCallback(HWND Window,
+                        UINT Message,
+                        WPARAM WParam,
+                        LPARAM LParam)
 {
     LRESULT Result = 0;
 
@@ -13,25 +67,31 @@ MainWindowCallback(HWND Window,
     {
     case WM_SIZE:
     {
-        printf("VM_SIZE\n");
-    }
-    break;
-
-    case WM_DESTROY:
-    {
-        printf("WM_DESTROY\n");
+        RECT ClientRect;
+        GetClientRect(Window, &ClientRect);
+        int Width = ClientRect.right - ClientRect.left;
+        int Height = ClientRect.bottom - ClientRect.top;
+        Win32ResizeDIBSection(Width, Height);
     }
     break;
 
     case WM_CLOSE:
     {
-        printf("WM_CLOSE\n");
+        // TODO: Handle this with a message to the user?
+        Running = false;
     }
     break;
 
     case WM_ACTIVATEAPP:
     {
         printf("WM_ACTIVATEAPP\n");
+    }
+    break;
+
+    case WM_DESTROY:
+    {
+        // TODO: Handle this as an error to the user?
+        Running = false;
     }
     break;
 
@@ -43,16 +103,7 @@ MainWindowCallback(HWND Window,
         int Y = Paint.rcPaint.top;
         int Width = Paint.rcPaint.right - Paint.rcPaint.left;
         int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-        static DWORD Operation = WHITENESS;
-        PatBlt(DeviceContext, X, Y, Width, Height, Operation);
-        if (Operation == WHITENESS)
-        {
-            Operation = BLACKNESS;
-        }
-        else
-        {
-            Operation = WHITENESS;
-        }
+        Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
         EndPaint(Window, &Paint);
     }
     break;
@@ -76,7 +127,7 @@ WinMain(HINSTANCE Instance,
 {
     WNDCLASSA WindowClass = {};
 
-    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.lpfnWndProc = Win32MainWindowCallback;
     WindowClass.hInstance = Instance; // or GetModuleHandle(0);
     // WindowClass.hIcon;
     WindowClass.lpszClassName = "HandmadeHeroWindowClass";
@@ -99,9 +150,10 @@ WinMain(HINSTANCE Instance,
                 0);
         if (WindowHandle)
         {
-            MSG Message;
-            for (;;)
+            Running = true;
+            while (Running)
             {
+                MSG Message;
                 BOOL MessageResult = GetMessageA(&Message, 0, 0, 0);
                 if (MessageResult > 0)
                 {
